80a22a0d93e7f128826f702d9bcc8cd54197c1c0
Size01 <59342780+Sirze01@users.noreply.github.com>
2020-11-19 10:08:16 +0000

    Documented some code

--- src/HouseMaster.h
+++ src/HouseMaster.h
@@ -37,18 +37,41 @@ public:
      * \return Unordered map of clients*/
     std::unordered_map<std::string, Client *>& getClients();
     /**A normal member taking no arguments and returning a vector of interventions
-     * \return Unordered map of collaborators*/
+     * \return A vector with all the interventions ordered by the time of the request*/
     std::vector<Intervention *>& getInterventions();
+    /**Void method to add a new Service to the HouseMaster's portfolio*/
     void addAvailableService(const std::string& name, bool pro, float basePrice, duration duration);
+    /**Method to remove a Service from HouseMaster's portfolio
+     * @param serviceName String containing the service to be removed*/
     void removeAvailableService(const std::string& serviceName);
+    /**A normal member taking no arguments and returning an unordered map of available Services
+     * Also referred as HouseMaster's Portfolio
+     * \return Unordered map of available Services*/
     std::unordered_map<std::string, Service*>& getAvailableServices();
+    /**Void method to add a new Collaborator to the HouseMaster's associates wallet
+     * @param collab* Pointer to the new Collaborator*/
     void addCollaborator(Collaborator *collab);
     //void addCollaborator(const std::string& username, Collaborator* collaborator);
+    /**Method to remove a Collaborator from HouseMaster's associates wallet
+     * @param collId String containing the Collaborator's ID*/
     void removeCollaborator(const std::string& collId);
+    /**Void method to add a new Collaborator to the HouseMaster's associates wallet
+     * @param functions Vector of strings containing the names of the Services the Collaborator can preform
+     * @param name String with the name of the new Collaborator
+     * @param pro Boolean describing if the Collaborator has professional credentials*/
     void addCollaborator(const std::vector<std::string> &functions, const std::string &name, bool pro);
     //void deleteCollaborator(const std::string& collId);
+    /**Void method to add a new Client to the database
+     * @param client* Pointer to the new Client*/
     void addClient(Client *client);
+    /**Void method to add a new Collaborator to the HouseMaster's associates wallet
+     * @param nif Tax payer number
+     * @param name String with the name of the new Collaborator
+     * @param premium Boolean describing if the Client requires all it's Interventions to be preformed by a Collaborator
+     * with professional credentials*/
     void addClient(unsigned int nif, const std::string &name, bool premium);
+    /**Method to remove a Client from HouseMaster's database
+     * @param clientId String containing the Client's ID*/
     void removeClient(const std::string& clientId);
     Intervention* addIntervention(const date& appointment, const std::string& type, bool forcePro, const std::string &clientId, unsigned int nrOfRooms=0);
     static void changeinterventionState(Intervention* intervention, processState state);
