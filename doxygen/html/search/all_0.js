e775df57fb01ac01272c9bfc09e66402a628ce46
Sofia Teixeira <anasofiaoliveirateixeira2@gmail.com>
2020-11-22 21:59:09 +0000

    doxygen updated

--- doxygen/html/search/all_0.js
+++ doxygen/html/search/all_0.js
@@ -6,7 +6,7 @@ var searchData=
   ['addclient_3',['addClient',['../class_house_master.html#a00d926f61b224fae7dd935194a4a9a51',1,'HouseMaster']]],
   ['addcollaborator_4',['addCollaborator',['../class_house_master.html#a33dcb71b0a60f52b78bc72021fe562cd',1,'HouseMaster']]],
   ['addintervention_5',['addIntervention',['../class_house_master.html#a2bc30f1631850e067a361f6f50b60aa6',1,'HouseMaster']]],
-  ['addservice_6',['addService',['../class_collaborator.html#ac9588bdcca9a5daf25464e4bf8cf5701',1,'Collaborator']]],
+  ['addservice_6',['addService',['../class_collaborator.html#ad0b209a80758e6701cf69c4ae12d2d6b',1,'Collaborator']]],
   ['alreadyknows_7',['AlreadyKnows',['../class_collaborator_1_1_already_knows.html',1,'Collaborator::AlreadyKnows'],['../class_collaborator_1_1_already_knows.html#a1a24ea3211a2f7bce04dd73a6aa8e346',1,'Collaborator::AlreadyKnows::AlreadyKnows()']]],
   ['assigncollaborator_8',['assignCollaborator',['../class_house_master.html#ad71a6fb1792f0b55039c8d25cdcbbecd',1,'HouseMaster']]],
   ['assignedcollab_9',['AssignedCollab',['../class_house_master_1_1_assigned_collab.html',1,'HouseMaster::AssignedCollab'],['../class_house_master_1_1_assigned_collab.html#a936e19e1a1fe0e4c985549f3ca3deab4',1,'HouseMaster::AssignedCollab::AssignedCollab()']]]
--- doxygen/html/search/functions_0.js
+++ doxygen/html/search/functions_0.js
@@ -6,7 +6,7 @@ var searchData=
   ['addclient_141',['addClient',['../class_house_master.html#a00d926f61b224fae7dd935194a4a9a51',1,'HouseMaster']]],
   ['addcollaborator_142',['addCollaborator',['../class_house_master.html#a33dcb71b0a60f52b78bc72021fe562cd',1,'HouseMaster']]],
   ['addintervention_143',['addIntervention',['../class_house_master.html#a2bc30f1631850e067a361f6f50b60aa6',1,'HouseMaster']]],
-  ['addservice_144',['addService',['../class_collaborator.html#ac9588bdcca9a5daf25464e4bf8cf5701',1,'Collaborator']]],
+  ['addservice_144',['addService',['../class_collaborator.html#ad0b209a80758e6701cf69c4ae12d2d6b',1,'Collaborator']]],
   ['alreadyknows_145',['AlreadyKnows',['../class_collaborator_1_1_already_knows.html#a1a24ea3211a2f7bce04dd73a6aa8e346',1,'Collaborator::AlreadyKnows']]],
   ['assigncollaborator_146',['assignCollaborator',['../class_house_master.html#ad71a6fb1792f0b55039c8d25cdcbbecd',1,'HouseMaster']]],
   ['assignedcollab_147',['AssignedCollab',['../class_house_master_1_1_assigned_collab.html#a936e19e1a1fe0e4c985549f3ca3deab4',1,'HouseMaster::AssignedCollab']]]
